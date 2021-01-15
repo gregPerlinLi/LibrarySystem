92425c4155c51c5b56a0b64a692bae021dea00cc
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2021-01-15 20:46:37 +0800

    fix(): Beautify the code

--- src/com/gregperlinli/service/GenericFunction.java
+++ src/com/gregperlinli/service/GenericFunction.java
@@ -94,7 +94,7 @@ public class GenericFunction {
         } finally {
             JDBCUtills.closeResource(conn, null);
         }
-
+                                      
         return list;
     }
 
